6a74dbf5fd52f654bea900adbd6dbd004f4c6e7f
Xinghe Lu <Xinghe Lu>
2016-04-12 22:28:38 -0400

    first commit

---
+++ app/src/main/java/com/lu_xinghe/project600final/NewsPageActivity.java
@@ -0,0 +1,13 @@
+package com.lu_xinghe.project600final;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_page);
+    }
+}
